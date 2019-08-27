365ff8d9bc6c65bfc72e45997cd888b7ce6ca13c
WSINTRA <will.simpson85@gmail.com>
2019-08-27 11:21:53 -0400

    first commit, still building out app

---
+++ DutchComponent.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const DutchComponent = () => (
+  <svg width={362} height={204} viewBox="0 0 362 204">
+    <defs>
+      <style>{".a{fill:url(#a);}"}</style>
+      <pattern
+        id="a"
+        preserveAspectRatio="none"
+        width="100%"
+        height="100%"
+        viewBox="0 0 560 315"
+      >
+        <image width={560} height={315} xlinkHref="ComponentTMP_1-image.png" />
+      </pattern>
+    </defs>
+    <rect className="a" width={362} height={204} />
+  </svg>
+);
+
+export default DutchComponent;

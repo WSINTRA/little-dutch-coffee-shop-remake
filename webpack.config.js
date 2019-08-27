bb35717892c938ff204d5eb0f27b83584008df05
WSINTRA <will.simpson85@gmail.com>
2019-08-27 12:12:41 -0400

    project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-	entry: __dirname + '/app/index.js',
-	module: {
-		loaders: [ 
-		{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'
-		]
-	},
-	output: {
-		filename: 'transformed.js',
-		path: __dirname + 'build'
-	}
-};
+   entry: './app/index.js',
+   output: {
+      path: path.join(__dirname, './bundle'),
+      filename: 'index_bundle.js'
+   },
+   devServer: {
+      inline: true,
+      port: 8001
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   },
+   plugins:[
+      new HTMLWebpackPlugin({
+         template: './app/index.html'
+      })
+   ]
+}
+
+
+
+

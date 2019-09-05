19ba45bd3f20e8f32c612c48d5c8acebf38f027f
WSINTRA <will.simpson85@gmail.com>
2019-09-05 14:05:36 -0400

    sass files refactored for scalability

--- src/components/LandingBox.js
+++ src/components/LandingBox.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const LandingBox = (props) => (
  <div className="landing"> 
-  <h1>Online dispensary for all your cannabis needs</h1>
-  <h2>You must be 21 or older to enter and live in a state in which recreational usage is legal to order</h2>
+  <h1>Online dispensary for CBD & THC based products</h1>
+  <h2>You must be 21 or older to enter and order</h2>
   <a href="#login"><div className="landing__login">LOGIN</div></a>
   <a href="#register"><div className="landing__register">REGISTER</div></a>
 </div>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, {keyframes} from 'styled-components';
-import {fadeInUp} from 'react-animations';
+import {fadeIn} from 'react-animations';
 import BackDrop from './BackDrop'
 import MenuHeader from './MenuHeader'
 import Header from './Header'
@@ -8,16 +8,16 @@ import LandingBox from './LandingBox'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
 
-const Bounce = styled.div`animation: 0.5s ${keyframes`${fadeInUp}`} 1`;
+const Bounce = styled.div`animation: 1.8s ${keyframes`${fadeIn}`} 1`;
 
 const LandingPage = (props) => (
 
- <div> 
- {props.menuOpen ? <Bounce><MenuHeader menuButton={props.openCloseMenu}/></Bounce>: 
+ <div> <Bounce>
+ {props.menuOpen ? <MenuHeader menuButton={props.openCloseMenu}/>: 
 	<Header menuButton={props.openCloseMenu}/>} 
     <BackDrop/><LandingBox/>
     <LoginForm/><RegisterForm/>
-
+</Bounce>
 </div>
 );
 

be4c9f41a7fa55155828eeb458b0a052b3273b35
WSINTRA <will.simpson85@gmail.com>
2019-11-05 13:54:58 -0500

    Simple styling applied to ProductDetails component

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -6,13 +6,14 @@ const Bounce = styled.div`animation: 2.8s ${keyframes`${fadeInRight}`} 1`;
 const ProductDetails = (props) => {
 	
 	return (
-	<Bounce>
+
     <div className="product-details">    
     {console.log(props)}
-
-   
+    <div>
+    {props.product.description}
+   </div>
     </div>
-     </Bounce>
+   
 
 	)}
 

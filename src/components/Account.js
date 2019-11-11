d8070a577fd2dd7d4bbb626b29c67968a5b98673
WSINTRA <will.simpson85@gmail.com>
2019-11-10 22:27:38 -0500

    previous orders table now visible for logged in customer

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from 'react-redux'
 import AdminProducts from './AdminProducts'
 import Customers from './Customers'
+import { Button, Header, Image, Table } from 'semantic-ui-react';
+
 const displayOption=(option)=>{
 	switch(option){
 		case "Sales stats":
@@ -25,6 +27,11 @@ const clickActionForOptions=(e,props)=>{
 	props.activeOptionSelect(e.target.innerText)
 }
 
+const orderDate=(date)=>{
+  let newDate = new Date(date)
+  return newDate.toDateString()
+}
+
 const Account = (props) => {
 	
 	return (
@@ -32,7 +39,7 @@ const Account = (props) => {
     <div className="account">
 
     <div className="banner">
-    {props.banner}
+    {props.banner} : {props.currentUser.username}
     </div>
 
    {props.currentUser.staff ?   <div>
@@ -63,8 +70,52 @@ const Account = (props) => {
    </div>
     </div>
    </div> :
- null 
+ <div>{console.log(props.currentUser)}
+     <div className="responsive_view">TEST RESPONSIVE</div>
+     <div className="account__content">
+     <div className="account__content_grid_items" >
+     <div className="active-option"><img src={props.currentUser.avatar}/>
+     <div style={{height:"19rem",overflow: "scroll"}}><h3>Purchase History</h3>
+     
+     
+
+       {props.currentUser.orders.map(order=> <React.Fragment>
+         <Table basic='very' celled collapsing>
+    <Table.Header>
+      <Table.Row >
+        <Table.HeaderCell>Order No.{order.id}</Table.HeaderCell>
+         <Table.HeaderCell>Product Name</Table.HeaderCell>
+          <Table.HeaderCell>Purchase Date</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+         
+        
+        {order.products.map(product=>
+          <React.Fragment><Table.Row >
+         <Table.Cell>
+       
+        </Table.Cell><Table.Cell>{product.title}</Table.Cell>
+          <Table.Cell>{orderDate(order.created_at)}</Table.Cell> </Table.Row></React.Fragment>
+        )}
+        
+        
+       
+         </Table.Body>
+      </Table>
+        </React.Fragment>)} 
+        
+      
+        
+      
+ 
+  
+ </div>
+      </div>
+     </div>
+  </div></div>
 // //Fill in how the account page should look for a normal user
+
 }
  
     </div>
